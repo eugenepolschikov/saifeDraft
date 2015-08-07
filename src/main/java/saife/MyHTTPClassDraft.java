584ea08e1f76984d30403b3f6d420d3ee0685d53
epolschikov <ypolshchykau@exadel.com>
2015-08-07 13:51:10 +0300

    added a couple of implemented in jMeter HTTP requests

--- src/main/java/saife/MyHTTPClassDraft.java
+++ src/main/java/saife/MyHTTPClassDraft.java
@@ -1,7 +1,16 @@
 package saife;
+import com.ning.http.client.*;
+import java.util.concurrent.Future;
 
 /**
  * Created by ypolshchykau on 05.08.2015.
  */
 public class MyHTTPClassDraft {
+    public MyHTTPClassDraft(){
+
+    }
+
+    public void testMethod(){
+
+    }
 }
